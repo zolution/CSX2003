c8bcc835c4b75b46a38d9e3961c55cda9fc9d1d1
zolution <zolution@outlook.com>
2018-03-13 19:55:57 +0800

    Add notes

--- app.js
+++ app.js
@@ -4,9 +4,21 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 
 // Database Name
-const dbName = 'myproject';
+const dbName = 'School';
 
 // Use connect method to connect to the server
+/*
+var connectionCallback = function(error, client){
+    console.log("Connected successfully to server");
+    const db = client.db(dbName);
+    client.close();
+}
+
+MongoClient.connect(url, connectionCallback);
+*/
+
+// anonymous function:
+/*
 MongoClient.connect(url, function(err, client) {
 //  assert.equal(null, err);
   console.log("Connected successfully to server");
@@ -15,3 +27,20 @@ MongoClient.connect(url, function(err, client) {
 
   client.close();
 });
+*/
+
+//java lambda
+MongoClient.connect(url, (err, client) => {
+//  assert.equal(null, err);
+  if(err){
+    console.log("Connection Fail");
+    return;
+  }
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  client.close();
+});
+
+
